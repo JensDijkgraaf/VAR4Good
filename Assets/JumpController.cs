216f2f84384cf38aacf28f6fbf7120bcc1df7d52
Jason Jheeta <jason.jheeta@hva.nl>
2024-01-11 13:08:34 +0100

    Jump script added

---
+++ Assets/JumpController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class JumpController : MonoBehaviour
+{
+    private CharacterController controller;
+    [SerializeField] private InputActionReference jumpButton;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField] private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        jumpButton.action.performed += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        jumpButton.action.performed -= OnJump;
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (groundedPlayer) 
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
