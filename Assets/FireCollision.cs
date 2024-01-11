ecdf7a5b40790c13b7bbc165cc817e35603dd104
annes <76728727+annesnour03@users.noreply.github.com>
2024-01-11 14:25:11 +0100

    Add explosions

--- Assets/FireCollision.cs
+++ Assets/FireCollision.cs
@@ -1,18 +1,25 @@
+using System;
 using UnityEngine;
 
-public class debugcol : MonoBehaviour {
+public class FireCollision : MonoBehaviour {
     [SerializeField, Range(0, 5)]
     private float velocity_threshold;
 
+    private ParticleSystem particles;
+    private void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+        if(particles is not null)
+            particles.Stop();
+    }
+
     private void OnCollisionEnter(Collision collision) {
-        float a = 1;
-        var test = collision.collider.gameObject.name;
+        // TODO enter a correct tag!
         if (collision.collider.gameObject.tag.Equals("test")) {
             float mag = collision.relativeVelocity.magnitude;
             // Now that we have the magnitude, we can check if it was big enough
             if (mag > velocity_threshold) {
-                // TODO: here spawn sparks/firea
-                a = 0;
+                particles.Play();
             }
         }
 
