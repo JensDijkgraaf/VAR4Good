0100a3ddd6b0804cb2862c0f95a922e2eef0127e
JensDijkgraaf <103207323+JensDijkgraaf@users.noreply.github.com>
2024-01-16 15:24:53 +0100

    Trees can be on fire and fire will spread between neighboring trees. Burnt down trees will fall and be destroyed (TODO: leave burn remains of trees)

--- Assets/Scripts/AxeInteraction.cs
+++ Assets/Scripts/AxeInteraction.cs
@@ -7,11 +7,6 @@ public class AxeInteraction : MonoBehaviour
     [SerializeField, Tooltip("Sound for tree hit")]
     private AudioClip treeHitSound;
 
-    [SerializeField, Tooltip("Sound for tree falling")]
-    private AudioClip treeFallSound;
-
-    [SerializeField, Tooltip("Prefab for the log")]
-    private GameObject logPrefab;
 
     private GameObject Player;
 
@@ -77,65 +72,12 @@ public class AxeInteraction : MonoBehaviour
                     {
                         hitCount = 0;
                         // Trigger tree falling sequence
-                        StartCoroutine(FallTree(other.gameObject));
+                        StartCoroutine(other.gameObject.GetComponent<TreeController>().FallTree());
                     }
 
                     lastHitTime = Time.time;
                 }
             }
-        }
-    }
-
-    private IEnumerator FallTree(GameObject tree)
-    {
-        // Play the tree falling sound
-        if (treeFallSound != null && audioSource != null)
-        {
-            audioSource.PlayOneShot(treeFallSound);
-        }
-
-        float fallDuration = 5f;
-        float rotationSpeedX = 90f / fallDuration;
-        float rotationSpeedZ = 90f / fallDuration;
-
-        Quaternion startRotation = tree.transform.rotation;
-        Quaternion targetRotation = Quaternion.Euler(startRotation.eulerAngles.x + 90f, startRotation.eulerAngles.y, startRotation.eulerAngles.z + 90f);
-
-        float elapsedTime = 0f;
-
-        while (elapsedTime < fallDuration)
-        {
-            tree.transform.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsedTime / fallDuration);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        tree.transform.rotation = targetRotation;
-
-        // Spawn logs after the tree falls
-        SpawnLogs(tree.transform.position);
-
-        // Destroy the tree
-
-        Destroy(tree);
-    }
-
-    private void SpawnLogs(Vector3 spawnPosition)
-    {
-        // Spawn a random number of logs between 1 and 4
-        int logCount = Random.Range(1, 5);
-
-        for (int i = 0; i < logCount; i++)
-        {
-            // Duplicate the logPrefab
-            GameObject newLog = Instantiate(logPrefab, spawnPosition, Quaternion.identity);
-
-            newLog.transform.position = new Vector3(spawnPosition.x + Random.Range(-0.5f, 0.5f), spawnPosition.y + 1, spawnPosition.z + Random.Range(-0.5f, 0.5f));
-            float randomRotationY = Random.Range(0f, 360f);
-            newLog.transform.rotation = Quaternion.Euler(0f, randomRotationY, 0f);
-        
-            // Set a unique name for each log
-            newLog.name = "Log_" + i;
         }
     }
 }
