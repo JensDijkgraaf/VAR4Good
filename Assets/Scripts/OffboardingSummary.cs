3c156abfeae14cb8856ca1dcba995244b8881831
Jason Jheeta <jason.jheeta@student.uva.nl>
2024-01-26 11:51:42 +0100

    disable screen & wristui after next button onclick + changed transform xr origin instead of two cameras

--- Assets/Scripts/OffboardingSummary.cs
+++ Assets/Scripts/OffboardingSummary.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
--- Assets/Scripts/ViewChanger.cs
+++ Assets/Scripts/ViewChanger.cs
@@ -8,41 +8,47 @@ public class ViewChanger : MonoBehaviour
     private float fadeDuration = 10f;
     public Color fadeColor;
     [SerializeField] public Camera mainCamera;
-    [SerializeField] public Camera overheadCamera;
-    private Renderer bottomRend;
-    private Renderer topRend;
+    private Renderer renderer;
     private DateTime currentTime;
     private DateTime endOfDay;
 
     private ActionBasedContinuousMoveProvider actionBasedContinuousMoveProvider;
     private ActionBasedContinuousTurnProvider actionBasedContinuousTurnProvider;
+    private CharacterController characterController;
+    private JumpController jumpController;
+    private GameObject wristCanvas;
 
     private OffboardingSummary offboardingSummary;
 
 
     private void Start()
     {
-        bottomRend  = mainCamera.transform.GetChild(0).GetComponent<Renderer>();
-        topRend = overheadCamera.transform.GetChild(0).GetComponent<Renderer>();
+        renderer = mainCamera.transform.GetChild(0).GetComponent<Renderer>();
 
         offboardingSummary = GameObject.Find("Offboarding").GetComponent<OffboardingSummary>();
 
         //Player
         actionBasedContinuousMoveProvider = GameObject.Find("XR Origin (XR Rig)").GetComponent<ActionBasedContinuousMoveProvider>();
         actionBasedContinuousTurnProvider = GameObject.Find("XR Origin (XR Rig)").GetComponent<ActionBasedContinuousTurnProvider>();
-
+        characterController = transform.GetComponent<CharacterController>();
+        jumpController = transform.GetComponent<JumpController>();
+        wristCanvas = GameObject.Find("WristCanvas");
     }
 
     private void FreezePlayer()
     {
         actionBasedContinuousMoveProvider.enabled = false;
         actionBasedContinuousTurnProvider.enabled = false;
+        characterController.enabled = false;
+        jumpController.enabled = false;
     }
 
     private void UnFreezePlayer()
     {
         actionBasedContinuousMoveProvider.enabled = true;
         actionBasedContinuousTurnProvider.enabled = true;
+        characterController.enabled = true;
+        jumpController.enabled = true;
     }
 
     private void TransitionToOverheadSubtask()
@@ -52,6 +58,8 @@ public class ViewChanger : MonoBehaviour
         // Freeze the player position.
         FreezePlayer();
 
+        wristCanvas.SetActive(false);
+
         offboardingSummary.ShowSummary();
 
         FadeOutTopRend();
@@ -60,7 +68,7 @@ public class ViewChanger : MonoBehaviour
     // Switches the camera to the overhead one, with a fade in/out.
     public void TransitionToOverhead()
     {
-        Fade(0, 1, bottomRend, this.TransitionToOverheadSubtask);
+        Fade(0, 1, this.TransitionToOverheadSubtask);
     }
 
     // public void TransitionToGround()
@@ -70,15 +78,15 @@ public class ViewChanger : MonoBehaviour
 
     private void FadeOutTopRend()
     {
-        Fade(1, 0,topRend);
+        Fade(1, 0);
     }
 
 
-    private void Fade(float alphaIn,float alphaOut, Renderer rend, Action endCall = null)
+    private void Fade(float alphaIn,float alphaOut,Action endCall = null)
     {
-        StartCoroutine(FadeRoutine(alphaIn,alphaOut,rend, endCall));
+        StartCoroutine(FadeRoutine(alphaIn,alphaOut,endCall));
     }
-    public IEnumerator FadeRoutine(float alphaIn,float alphaOut,Renderer rend, Action endCall = null)
+    public IEnumerator FadeRoutine(float alphaIn,float alphaOut,Action endCall = null)
     {
         float time = 0;
         while(time <=  fadeDuration){
@@ -86,26 +94,26 @@ public class ViewChanger : MonoBehaviour
             var color = fadeColor;
             color.a = Mathf.Lerp(alphaIn, alphaOut, Mathf.Clamp01(time / fadeDuration));
 
-            rend.material.SetColor("_Color",color);
+            renderer.material.SetColor("_Color",color);
             time += Time.deltaTime;
             yield return null;
         }
         var color2 = fadeColor;
         color2.a = alphaOut;
-        rend.material.SetColor("_Color",color2) ;
+        renderer.material.SetColor("_Color",color2) ;
         if(endCall != null)
             endCall();
     }
     
     public void ShowOverheadView()
     {
-        mainCamera.enabled = false;
-        overheadCamera.enabled = true;
+        transform.position = new Vector3((float)44.32, (float)36.22, (float)18.13);
+        transform.eulerAngles = new Vector3(30, -120, 0);
     }
 
-    public void ShowMainCamera()
+    public void ShowGroundView()
     {
-        mainCamera.enabled = true;
-        overheadCamera.enabled = false;
+        transform.position = new Vector3((float)12.83, (float)0.0, (float)33.97);
+        transform.eulerAngles = new Vector3(0, 0, 0);
     }
 }
