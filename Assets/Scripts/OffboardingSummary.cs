b058ae37a26b6590600c1a95c647172f9c8dd5fa
Jason Jheeta <jason.jheeta@student.uva.nl>
2024-01-26 14:10:55 +0100

    disabled offboarding at start

--- Assets/Scripts/OffboardingSummary.cs
+++ Assets/Scripts/OffboardingSummary.cs
@@ -9,17 +9,19 @@ public class OffboardingSummary : MonoBehaviour
 {
     private ScoreController scoreController;
     [SerializeField] private TextMeshPro offboardingText;
+    private GameObject screen;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreController = GameObject.Find("Player").GetComponent<ScoreController>();
+        screen = transform.gameObject;
+        screen.SetActive(false);
     }
 
     public void ShowSummary()
     {
         string summary = scoreController.GetSummary();
-        GameObject screen = transform.gameObject;
         screen.SetActive(true);
         offboardingText.text = summary;
     }
