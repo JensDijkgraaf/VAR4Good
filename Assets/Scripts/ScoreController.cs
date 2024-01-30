fa9e697531225f5e14ef417b4353033a5e7cb8e0
JensDijkgraaf <103207323+JensDijkgraaf@users.noreply.github.com>
2024-01-30 11:23:54 +0100

    updated score controller to work properly

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -27,6 +27,9 @@ public enum Actions
 
     [CustomString("Campfire burned 10 seconds too long")]
     CAMPFIRE_BURNED_LONG,
+
+    [CustomString("Campfire set on fire without enough stones")]
+    CAMPFIRE_SET_ON_FIRE_WITHOUT_STONES,
 }
 
 public class ScoreController : MonoBehaviour
@@ -34,13 +37,20 @@ public class ScoreController : MonoBehaviour
     [SerializeField, Tooltip("Starting score for the player"), Range(0, 100)]
     private int startScore = 0;
 
-    private readonly Dictionary<Actions, int> _offenseWeight = new();
-    private readonly Dictionary<Actions, int> _totalOffenses = new();
+    private static readonly Dictionary<Actions, int> _offenseWeight = new Dictionary<Actions, int>
+    {
+        { Actions.TREE_HIT, 1 },
+        { Actions.BEE_KILLED, 2 },
+        { Actions.TREE_FIRE, 4 },
+        { Actions.NOT_LOOKING_FIRE, 2 },
+        { Actions.CAMPFIRE_BURNED_LONG, 2 },
+        { Actions.CAMPFIRE_SET_ON_FIRE_WITHOUT_STONES, 2 }
+    };    private readonly Dictionary<Actions, int> _totalOffenses = new();
 
     // Start is called before the first frame update
     private void Start()
     {
-        InitializeDictionary();
+        // InitializeDictionary();
     }
 
     private void InitializeDictionary()
@@ -50,6 +60,7 @@ public class ScoreController : MonoBehaviour
         _offenseWeight.Add(Actions.TREE_FIRE, 4);
         _offenseWeight.Add(Actions.NOT_LOOKING_FIRE, 2);
         _offenseWeight.Add(Actions.CAMPFIRE_BURNED_LONG, 2);
+        _offenseWeight.Add(Actions.CAMPFIRE_SET_ON_FIRE_WITHOUT_STONES, 2);
     }
 
     // Public methods
@@ -61,13 +72,11 @@ public class ScoreController : MonoBehaviour
     public void TreeOnFire()
     {
         TrackAction(Actions.TREE_FIRE);
-
     }
     
     public void NotLookingAtFire()
     {
         TrackAction(Actions.NOT_LOOKING_FIRE);
-
     }
 
     public void BeeKilled()
@@ -80,6 +89,11 @@ public class ScoreController : MonoBehaviour
         TrackAction(Actions.CAMPFIRE_BURNED_LONG);
     }
 
+    public void CampfireSetOnFireWithoutStones()
+    {
+        TrackAction(Actions.CAMPFIRE_SET_ON_FIRE_WITHOUT_STONES);
+    }
+
     public void CampfireSetOnFire()
     {
         startScore += 100;
@@ -90,11 +104,11 @@ public class ScoreController : MonoBehaviour
         int points = _offenseWeight[action];
         if (_totalOffenses.ContainsKey(action))
         {
-            _totalOffenses[action] += points;
+            _totalOffenses[action] += 1;
         }
         else
         {
-            _totalOffenses[action] = points;
+            _totalOffenses[action] = 1;
         }
     }
 
